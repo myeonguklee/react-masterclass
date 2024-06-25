7e4c6353fccce92a93ecdc71eef6e2f84c8efcd1
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-06-25 16:12:20 +0900

    adapt_extending

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
 
 const Text = styled.span`
   color: whith;
@@ -23,10 +21,8 @@ const Text = styled.span`
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
     </Father>
   );
 }
