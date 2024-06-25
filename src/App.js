c404c9559d649a303b509d65cc9abca575d8f598
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-06-25 16:47:20 +0900

    as_attrs

--- src/App.js
+++ src/App.js
@@ -18,13 +18,31 @@ const Text = styled.span`
   color: whith;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`
+
+const Input = styled.input.attrs({ required: true, minLength: 10})`
+  background-color: tomato;
+`;
+
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
 
 export default App;
+
+/* <Box bgColor="teal"/>
+<Circle bgColor="tomato"/> */
