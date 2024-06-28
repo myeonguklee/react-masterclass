55220cac1bd516b888e27d279fae06c451ca7638
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-06-28 09:38:32 +0900

    before:react-query

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,14 @@
 
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
 };
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
-};
+// export const darkTheme: DefaultTheme = {
+//   bgColor: "black",
+//   textColor: "white",
+//   btnColor: "teal",
+// };
