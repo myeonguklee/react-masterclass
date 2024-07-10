8e29195edfd6f16f7fff5d3df2c27d7ab530a666
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-10 20:36:32 +0900

    multi_boards

---
+++ trello/src/Components/DragabbleCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div<{ isDragging: boolean }>`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px;
+  background-color: ${(props) =>
+    props.isDragging ? "#e4f2ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.05)" : "none"};
+`;
+
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  return (
+    <Draggable draggableId={toDo} index={index}>
+      {(magic, snapshot) => (
+        <Card
+          isDragging={snapshot.isDragging}
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DragabbleCard);
--- trello/src/atoms.tsx
+++ trello/src/atoms.tsx
@@ -1,2 +1,15 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
+interface IToDoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<IToDoState>({
+  key: "toDo",
+  default: {
+    "To Do": ["a", "b"],
+    Doing: ["c", "d", "e"],
+    Done: ["f"],
+    "Do Later": ["x", "z"],
+  },
+});
