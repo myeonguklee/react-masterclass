c4cbf72bb11085da5eb042a2c16abd2b402b587d
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-14 21:31:43 +0900

    motionvalues_scale

--- react-animations/src/App.tsx
+++ react-animations/src/App.tsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { start } from "repl";
-import { transcode } from "buffer";
-import { click } from "@testing-library/user-event/dist/click";
-import { hover } from "@testing-library/user-event/dist/hover";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -16,27 +13,23 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {backgroundColor: "rgb(46, 204, 113)", transition:{duration:10}}
-};
-
 function App() {
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800],[2, 1, 0.1])
+  useEffect(() => {
+    // x.onChange(() => console.log(x.get()));
+    scale.onChange(() => console.log(scale.get()));
+  }, [x]);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      ></Box>
+      {/* <button onClick={() => x.set(200)}>click me</button> */}
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
