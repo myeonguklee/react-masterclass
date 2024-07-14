1f8d965d047637cf1bd5786b44d557d72b52ea59
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-14 22:07:03 +0900

    motionvalues_scroll

--- react-animations/src/App.tsx
+++ react-animations/src/App.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 import { useEffect } from "react";
+import { Socket } from "dgram";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -13,7 +14,6 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -21,15 +21,29 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800],[2, 1, 0.1])
-  useEffect(() => {
-    // x.onChange(() => console.log(x.get()));
-    scale.onChange(() => console.log(scale.get()));
-  }, [x]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-400, 400],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  // useEffect(() => {
+  //   scrollY.onChange(() => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollYProgress, scrollY]);
   return (
-    <Wrapper>
-      {/* <button onClick={() => x.set(200)}>click me</button> */}
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x, rotateZ, scale}}
+        drag="x"
+        dragSnapToOrigin
+      />
     </Wrapper>
   );
 }
