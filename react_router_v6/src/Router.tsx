1a85019e9978121217d31cc716cc03418e081c24
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-28 23:15:24 +0900

    errorElement

--- react_router_v6/src/Router.tsx
+++ react_router_v6/src/Router.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./component/ErrorComponent";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <Root />,
-  children: [
-    {
-      path: "",
-      element: <Home />,
-    },
-    {
-      path:"about",
-      element: <About />  
-    },]
-}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
 
 export default router;
---
+++ react_router_v6/src/component/ErrorComponent.tsx
@@ -0,0 +1,10 @@
+
+function ErrorComponent() {
+  return (
+    <div>
+      <h1>This component crashed</h1>
+    </div>
+  );
+}
+
+export default ErrorComponent;
