c0345b477fe6fab54cf962e57d03298639b49a93
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-31 04:49:26 +0900

    useNavigate

--- react_router_v6/src/component/Header.tsx
+++ react_router_v6/src/component/Header.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const onAboutClick = () => {
+    const navigate = useNavigate();
+    const onAboutClick = () => {
+      navigate("/about");
+    };
+  };
+
   return (
     <header>
       <ul>
@@ -9,7 +16,7 @@ function Header() {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
