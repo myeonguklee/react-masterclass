b28e6f11c8008c78b721d66fa7389d898c90db08
이명욱 <muk525@icloud.com>
2024-07-31 08:46:32 +0900

    useOutletContext

--- react_router_v6/src/screens/users/Followers.tsx
+++ react_router_v6/src/screens/users/Followers.tsx
@@ -1,4 +1,12 @@
+import { useOutletContext } from "react-router-dom";
+
+interface IFollowersContext {
+  nameOfMyUser: string;
+}
+
 function Followers() {
-  return <div>Followers</div>;
+  const { nameOfMyUser } = useOutletContext<IFollowersContext>();
+  console.log(nameOfMyUser);
+  return <div>Here are {nameOfMyUser}의 followers</div>;
 }
 export default Followers;
